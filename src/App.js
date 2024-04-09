bd3075a0c83b0ed001c76504f2d81ce0d9decb7e
User <user@Users-MacBook-Air.local>
2024-04-09 14:00:46 +1000

    all

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import TimeLocation from './components/TimeLocation';
+
+function App() {
+  return (
+    <TimeLocation />
+  );
+}
+
+export default App;
---
+++ src/components/TimeLocation.jsx
@@ -0,0 +1,17 @@
+import "./timelocation.css"
+
+
+function TimeLocation () {
+    return (
+      <div className="timelocation">
+        <div className="weekndate">
+          <div className="week">Mon</div>
+          <div className="date">4.9</div>
+        </div>
+        <div className="timestamp">12:55pm</div>
+        <div className="location">Sydney</div>
+      </div>
+    );
+}
+
+export default TimeLocation
